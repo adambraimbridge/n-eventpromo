3713010ff3cfaebffc581b096ee825a1f3351d40
Tye, Thurston <Thurston.Tye@ft.com>
2018-05-09 17:20:41 +0100

    some basic setup

---
+++ js/main.js
@@ -0,0 +1,44 @@
+//import {broadcast} from 'n-ui-foundations';
+//const lazyLoadImages = require('n-image').lazyLoad;
+const eventPromoClient = require('./eventpromo-client');
+const template = require('../../../views/partials/event-promo/event-promo-inarticle.html');
+
+module.exports.init = () => {
+
+	//How do we define which slot to be used?
+	//const promoSlots = document.querySelectorAll('.js-event-promo');
+	const promoSlot = document.querySelector('.js-event-promo');
+	const eventTags = document.querySelector('#js-event-promo-data').innerHTML;
+
+	if(eventTags) {
+
+		eventPromoClient(eventTags)
+		.then((event) => {
+			//return the basics for rendering an event
+			if (event === undefined) {
+				return;
+			}
+
+			return {
+				id: event.id,
+				title: event.prefLabel,
+				cta: event.eventDetailsUrl,
+				mainImage: event._imageUrl,
+				start: event.scheduledStartTime,
+				eventUrl: event.eventURL,
+				eventFocus: 'Brexit' //TODO get this from the data returned
+			};
+
+		}).then(eventData => {
+			if(!eventData) {
+				return;
+			}
+			promoSlot.innerHTML = template(eventData);
+		})
+		.catch(error => {
+			throw error;
+		});
+	}
+
+	return;
+};
