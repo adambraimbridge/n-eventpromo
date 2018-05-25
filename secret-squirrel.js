9b47f340d91bba36247b3f5ff9e68ac69d44f5ff
Tye, Thurston <Thurston.Tye@ft.com>
2018-05-25 23:03:05 +0100

    add denyOverrides for demo fixtures  🐿 v2.8.0

--- secret-squirrel.js
+++ secret-squirrel.js
@@ -6,9 +6,9 @@ module.exports = {
 	strings: {
 		deny: [],
 		denyOverrides: [
-			'f17e63fd1eda51789dc5e6bdd8e3dfae', // demos/fixtures.json:2|2
-			'19b95057-4614-45fb-9306-4d54049354db', // demos/fixtures.json:17
-			'd90b783d-8eb9-30bb-b078-c0cef19bacb0' // demos/fixtures.json:21
+			// 'f17e63fd1eda51789dc5e6bdd8e3dfae', // demos/fixtures.json:3|3
+			// '19b95057-4614-45fb-9306-4d54049354db', // demos/fixtures.json:17
+			// 'd90b783d-8eb9-30bb-b078-c0cef19bacb0' // demos/fixtures.json:21
 		]
 	}
 };
