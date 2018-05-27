5bbdfcfe2434b76dadaea906ef7c42b277bd8eaa
Tye, Thurston <Thurston.Tye@ft.com>
2018-05-27 13:14:58 +0100

    switch to promise because of webpack asyn/await wierdness  🐿 v2.8.0

--- src/event-promo-client.js
+++ src/event-promo-client.js
@@ -1,21 +1,40 @@
-async function getEventsFromApi (eventConcepts = []) {
+const url = '/eventpromo/api/';
+
+// async function getEventsFromApi (eventConcepts = []) {
+// 	const url = '/eventpromo/api/';
+
+// 	try {
+// 		const httpResult = await fetch(url, {
+// 			body: JSON.stringify(eventConcepts),
+// 			headers: {
+// 				'accept': 'application/json',
+// 				'content-type': 'application/json'
+// 			},
+// 			method: 'POST'
+// 		});
+
+// 		return await httpResult.json();
+// 	}
+// 	catch(error) {
+// 		throw error;
+// 	}
+// }
+
+function getEventsFromApi (eventConcepts = []) {
 	const url = '/eventpromo/api/';
 
-	try {
-		const httpResult = await fetch(url, {
-			body: JSON.stringify(eventConcepts),
-			headers: {
-				'accept': 'application/json',
-				'content-type': 'application/json'
-			},
-			method: 'POST'
-		});
-
-		return await httpResult.json();
-	}
-	catch(error) {
-		throw error;
-	}
+	return fetch(url, {
+		body: JSON.stringify(eventConcepts),
+		headers: {
+			'accept': 'application/json',
+			'content-type': 'application/json'
+		},
+		method: 'POST'
+	})
+	.then((eventPromos) => eventPromos.json())
+	.catch(err => {
+		throw err;
+	});
 }
 
 module.exports = {
