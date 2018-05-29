0ed0e94bca9d18d8f0c978437b690d6f4860eab6
ludrob <robin.ludovic@gmail.com>
2018-05-29 16:08:30 +0100

    es6 changes  🐿 v2.8.0

--- src/event-promo-client.js
+++ src/event-promo-client.js
@@ -1,38 +1,23 @@
-// async function getEventsFromApi (eventConcepts = []) {
-// 	const url = '/eventpromo/api/';
-
-// 	try {
-// 		const httpResult = await fetch(url, {
-// 			body: JSON.stringify(eventConcepts),
-// 			headers: {
-// 				'accept': 'application/json',
-// 				'content-type': 'application/json'
-// 			},
-// 			method: 'POST'
-// 		});
-
-// 		return await httpResult.json();
-// 	}
-// 	catch(error) {
-// 		throw error;
-// 	}
-// }
-
-function getEventsFromApi (eventConcepts = []) {
+async function getEventsFromApi (eventConcepts = []) {
 	const url = '/eventpromo/api/';
 
-	return fetch(url, {
-		body: JSON.stringify(eventConcepts),
-		headers: {
-			'accept': 'application/json',
-			'content-type': 'application/json'
-		},
-		method: 'POST'
-	})
-	.then((eventPromos) => eventPromos.json())
-	.catch(err => {
+	try {
+		const fetchResponse = await fetch(url, {
+			body: JSON.stringify(eventConcepts),
+			headers: {
+				'accept': 'application/json',
+				'content-type': 'application/json'
+			},
+			method: 'POST'
+		});
+
+		const eventPromos = await fetchResponse.json();
+
+		return eventPromos;
+	}
+	catch (err) {
 		throw err;
-	});
+	}
 }
 
 module.exports = {
--- src/main.js
+++ src/main.js
@@ -3,44 +3,39 @@ const eventPromo = require('./event-promo-client');
 const template = require('../templates/inarticle.html');
 
 function mapEventData (theEvent) {
-	return new Promise((resolve, reject) => {
-		if (theEvent) {
-			const mappedEvent = {
-				id: theEvent.id,
-				eventTitle: theEvent.prefLabel,
-				cta: theEvent.eventDetailsUrl,
-				mainImage: encodeURI(theEvent._imageUrl),
-				eventStart: moment(theEvent.scheduledStartTime).format('D MMMM YYYY'),
-				eventUrl: theEvent.eventURL,
-				eventLocation: 'London' //TODO get this from the tags
-			};
-			resolve(mappedEvent);
-		} else {
-			reject('no event');
-		}
+	if (! theEvent) {
+		throw new Error('no event');
+	}
 
-	});
+	const mappedEvent = {
+		id: theEvent.id,
+		eventTitle: theEvent.prefLabel,
+		cta: theEvent.eventDetailsUrl,
+		mainImage: encodeURI(theEvent._imageUrl),
+		eventStart: moment(theEvent.scheduledStartTime).format('D MMMM YYYY'),
+		eventUrl: theEvent.eventURL,
+		eventLocation: 'London' //TODO get this from the tags
+	};
+	return mappedEvent;
 }
 
-module.exports = () => {
-
-	//How do we define which slot to be used?
+module.exports = async () => {
 	const promoSlot = document.querySelector('.js-event-promo');
 
 	if (document.querySelector('.js-event-promo-data')) {
 		const concepts = JSON.parse(document.querySelector('.js-event-promo-data').innerHTML);
 
-		return eventPromo.getEventsFromApi(concepts)
-			.then(promoEvents => {
-				return promoEvents.eventpromos[0];
-			})
-			.then(mapEventData)
-			.then(mappedEvent => {
-				return promoSlot.innerHTML = template(mappedEvent);
-			})
-			.catch(() => {
-				//fail silently
-				return;
-			});
+		try {
+			const promoEvents = await eventPromo.getEventsFromApi(concepts);
+			const mappedEvent = mapEventData(promoEvents.eventpromos[0]);
+
+			promoSlot.innerHTML = template(mappedEvent);
+		}
+		catch (err) {
+			//fail silently
+			return false;
+		}
+
+		return true;
 	}
 };
