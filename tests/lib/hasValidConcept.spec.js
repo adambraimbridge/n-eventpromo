4c4791a5031a4049da26b624c53186d5e3fb683d
Tye, Thurston <Thurston.Tye@ft.com>
2018-06-01 17:11:05 +0100

    spec for hasValidConcepts  🐿 v2.8.0

---
+++ tests/lib/hasValidConcept.spec.js
@@ -0,0 +1,14 @@
+const subject = require('../../src/lib/hasValidConcept');
+const validConcepts = require('../fixtures/conceptFixture');
+const invalidConceptObject = require('../fixtures/invalidConcepts');
+describe('hasValidConcept()', () => {
+
+	test('given a concept object with valid concept values returns true', () => {
+		expect(subject(validConcepts)).toBe(true);
+	});
+
+	test('given a concept object with invalid concept values returns false', () => {
+		expect(subject(invalidConceptObject)).toBe(false);
+	});
+
+});
