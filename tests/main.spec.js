d1820f82b143369eae4ad589e74d36cec1f8425a
Tye, Thurston <Thurston.Tye@ft.com>
2018-06-06 16:28:44 +0100

    100 test coverage  🐿 v2.8.0

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -7,6 +7,7 @@ const conceptFixture = JSON.stringify(require('./fixtures/conceptFixture.json'))
 const eventPromoDataEl = 	`<script class="js-event-promo-data" type="application/json">${conceptFixture}</script>
 <div class="event-promo js-event-promo" ></div>`;
 const noEventPromo = '<div class="no-event"></div>';
+const liveEvent = require('./fixtures/liveEvent.json');
 
 let dom = new JSDOM('<!DOCTYPE html><div class="event-promo-container"></div>');
 let document = dom.window.document;
@@ -14,12 +15,8 @@ let document = dom.window.document;
 
 describe('Unit tests: main', () => {
 
-	beforeEach(() => {
-
-	});
-
 	afterEach(() => {
-
+		fetchMock.restore();
 	});
 
 	describe('eventPromoInit method', () => {
@@ -106,10 +103,32 @@ describe('Unit tests: main', () => {
 		});
 		describe('success', () => {
 
-			test('should update dom', () => {
+			test('should update dom', async () => {
+				fetchMock.post('/eventpromo/api/', liveEvent);
+				const eventSource = liveEvent.eventpromos[0];
+				const eventContainer = document.querySelector('.event-promo-container');
+				eventContainer.innerHTML= eventPromoDataEl;
+
+				await eventPromoInit(document);
+
+				const injectedPromo = document.querySelector('.event-promo-inline');
+				const eventTitle = document.querySelector('.event-promo__title').innerHTML;
+				const eventCta = document.querySelector('.event-promo-inline__btn');
+
+
+				expect(injectedPromo).toBeTruthy();
+				expect(injectedPromo.childElementCount).toBeGreaterThan(0);
+				expect(eventTitle).toEqual(eventSource.prefLabel);
+				expect(eventCta.href).toEqual(eventSource.eventURL);
 
 			});
-			test('should return true', () => {
+			test('should return true', async () => {
+				const eventContainer = document.querySelector('.event-promo-container');
+
+				fetchMock.post('/eventpromo/api/', liveEvent);
+				eventContainer.innerHTML= eventPromoDataEl;
+
+				expect(await eventPromoInit(document)).toEqual(true);
 
 			});
 		});
