01f3e4cdefa0c49152218e9a114562e65b890ca9
Tye, Thurston <Thurston.Tye@ft.com>
2018-06-21 16:05:13 +0100

    bit of logic to switch variant template  🐿 v2.8.0

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -15,6 +15,12 @@ let document = dom.window.document;
 
 describe('Unit tests: main', () => {
 
+	beforeEach(() => {
+		global.FT = {
+			flags : {}
+		};
+	});
+
 	afterEach(() => {
 		fetchMock.restore();
 	});
@@ -114,6 +120,36 @@ describe('Unit tests: main', () => {
 		describe('success', () => {
 
 			test('should update dom', async () => {
+				fetchMock.post('/eventpromo/api/', liveEvent);
+				const eventSource = liveEvent.eventpromos[0];
+				const eventContainer = document.querySelector('.event-promo-container');
+				eventContainer.innerHTML = eventPromoDataEl;
+
+				await eventPromoInit(document);
+
+				const injectedPromo = document.querySelector('.event-promo-inline');
+				const eventTitle = document.querySelector('.event-promo__title').innerHTML;
+				const eventCta = document.querySelector('.event-promo-inline__btn');
+
+				expect(injectedPromo).toBeTruthy();
+				expect(injectedPromo.childElementCount).toBeGreaterThan(0);
+				expect(eventTitle).toEqual(eventSource.prefLabel);
+				expect(eventCta.href).toEqual(eventSource.eventURL);
+
+			});
+			test('should return true', async () => {
+				const eventContainer = document.querySelector('.event-promo-container');
+
+				fetchMock.post('/eventpromo/api/', liveEvent);
+				eventContainer.innerHTML = eventPromoDataEl;
+
+				expect(await eventPromoInit(document)).toEqual(true);
+			});
+		});
+
+		xdescribe('variant success', () => {
+
+			test('should update dom using variant template', async () => {
 				fetchMock.post('/eventpromo/api/', liveEvent);
 				const eventSource = liveEvent.eventpromos[0];
 				const eventContainer = document.querySelector('.event-promo-container');
