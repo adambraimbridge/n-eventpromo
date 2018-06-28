c764a7b6fe592bafaa74f6e7a3fcebeb9260488c
ludrob <robin.ludovic@gmail.com>
2018-06-28 12:44:41 +0100

    segment id changes, removed shortLabel  🐿 v2.8.0

--- src/lib/mapEventData.js
+++ src/lib/mapEventData.js
@@ -1,12 +1,16 @@
 const oDate = require('o-date');
 
 module.exports = (theEvent) => {
+	const eventUrl = new URL(theEvent.eventUrl);
+	eventUrl.searchParams.set('segmentId', theEvent.segmentId);
+
 	return {
 		id: theEvent.id,
 		eventTitle: theEvent.title,
 		mainImage: encodeURI(theEvent.imageUrl),
 		eventStart: oDate.format(theEvent.scheduledStartTime, 'dd MMMM yyyy'),
-		eventUrl: theEvent.eventUrl,
+		eventUrl: eventUrl.toString(),
+		segmentId: theEvent.segmentId,
 		eventLocation: theEvent.location
 	};
 };
--- tests/lib/mapEventData.spec.js
+++ tests/lib/mapEventData.spec.js
@@ -7,10 +7,13 @@ describe('mapEventData()', () => {
 
 		const subject = mapEventData(anEvent);
 
+		const expectedUrl = new URL(anEvent.eventUrl);
+		expectedUrl.searchParams.set('segmentId', anEvent.segmentId);
+
 		expect(subject).toHaveProperty('id', anEvent.id);
 		expect(subject).toHaveProperty('eventTitle', anEvent.title);
 		expect(subject).toHaveProperty('mainImage', encodeURI(anEvent.imageUrl));
-		expect(subject).toHaveProperty('eventUrl', anEvent.eventUrl);
+		expect(subject).toHaveProperty('eventUrl', expectedUrl.toString());
 		expect(subject).toHaveProperty('eventStart', '12 June 2018');
 		expect(subject).toHaveProperty('eventLocation', anEvent.location);
 	});
