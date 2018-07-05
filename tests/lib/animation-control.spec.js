1fe1162db5fc51ce8a4bdb972d7f761e492b2a2f
Tye, Thurston <Thurston.Tye@ft.com>
2018-07-05 22:02:47 +0100

    update tests  🐿 v2.9.0

---
+++ tests/lib/animation-control.spec.js
@@ -0,0 +1,6 @@
+describe('animation-control', () => {
+
+	it('should toggle animation-play-state', () => {
+		console.log('test me!');
+	});
+});
--- tests/lib/mapEventData.spec.js
+++ tests/lib/mapEventData.spec.js
@@ -1,13 +1,14 @@
 
 const anEvent = require('../fixtures/liveEvent.json').eventpromos[0];
+const imageArrayFixture = require('../fixtures/imagesArray');
 const mapEventData = require('../../src/lib/mapEventData');
 
 describe('mapEventData()', () => {
 	test('test it extract teaser details from eventpayload', () => {
 
 		const subject = mapEventData(anEvent);
-
 		const expectedUrl = new URL(anEvent.eventUrl);
+		const imageArray = [];
 		expectedUrl.searchParams.set('segmentId', anEvent.segmentId);
 
 		expect(subject).toHaveProperty('id', anEvent.id);
@@ -16,5 +17,31 @@ describe('mapEventData()', () => {
 		expect(subject).toHaveProperty('eventUrl', expectedUrl.toString());
 		expect(subject).toHaveProperty('eventStart', '12 June 2018');
 		expect(subject).toHaveProperty('eventLocation', anEvent.location);
+		expect(subject).toHaveProperty('segmentId', anEvent.segmentId);
+		expect(subject).toHaveProperty('images');
+		expect(subject).toHaveProperty('showVariant', false);
+		expect(subject.images).toEqual(expect.arrayContaining(imageArray));
+	});
+
+	//Relelvant to AB test propbably ok to remove amend on conclusion.
+	test('test provides the correct control properties', () => {
+
+		const subject = mapEventData(anEvent);
+		const expectedUrl = new URL(anEvent.eventUrl);
+		const imageArray = [];
+		expectedUrl.searchParams.set('segmentId', anEvent.segmentId);
+
+		expect(subject).toHaveProperty('showVariant', false);
+		expect(subject.images).toEqual(expect.arrayContaining(imageArray));
+	});
+
+	test('test provides the correct control properties', () => {
+
+		const subject = mapEventData(anEvent, 'variant');
+		const expectedUrl = new URL(anEvent.eventUrl);
+		expectedUrl.searchParams.set('segmentId', anEvent.segmentId);
+
+		expect(subject).toHaveProperty('showVariant', true);
+		expect(subject.images).toEqual(expect.arrayContaining(imageArrayFixture));
 	});
 });
