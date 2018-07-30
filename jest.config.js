d0cd10005d2d7ccc61aef0f19a7a052e23ad6ce6
Tye, Thurston <Thurston.Tye@ft.com>
2018-07-30 18:57:15 +0100

    set a test url to fix localStorage is not available issue  🐿 v2.9.0

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
 		'\\.html$': 'jest-handlebars'
 	},
 	coveragePathIgnorePatterns: ['/node_modules/', '/bower_components/'],
-	testEnvironment: 'node'
+	testEnvironment: 'node',
+	testURL: 'http://localhost/'
 };
