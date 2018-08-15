18fa6035b4748306a0391e7b922d143e76ed51b0
ludrob <robin.ludovic@gmail.com>
2018-08-15 16:17:22 +0100

    removed useless deps  🐿 v2.10.2

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-	transform: {
-		'\\.html$': 'jest-handlebars'
-	},
 	coveragePathIgnorePatterns: ['/node_modules/', '/bower_components/'],
 	//testEnvironment: 'node',
 	testURL: 'http://localhost/'
