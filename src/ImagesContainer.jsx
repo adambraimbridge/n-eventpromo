a3561f2d4b5fe974baf54912927af8298fabe91e
Nick Colley <nick.colley@ft.com>
2020-06-26 13:26:58 +0100

    Improve Image Slideshow button label
    Previously the label read `control animation`. Without context it's not clear what this means, what does control mean and what animation does it refer to?
    
    The new label will read 'Play "{title}" image slide show' for example 'Play "FT Brexit and Beyond Summit" image slideshow'.
    
    I have chosen the word `slideshow` as I hope this is more descriptive of the animation.
    
    If the animation is running it will say "Pause" instead of "Play".
    
    In the future, I agree with the Digital Accessibility Center (DAC) report that we should reconsider this animation all together, could we remove it and keep a single fixed image?
    
    DAC ID: IDAC_Non-Descriptive_Form_Elements_Issue4 🐿 v2.13.0

--- src/ImagesContainer.jsx
+++ src/ImagesContainer.jsx
@@ -11,15 +11,16 @@ const withPauseActions = withActions(() => ({
 	}
 }));
 
-const BaseImagesContainer = ({ isPaused, actions: { togglePause }, images, link }) => {
+const BaseImagesContainer = ({ isPaused, actions: { togglePause }, images, link, title }) => {
 	let classNames = styles['pause-button'];
 
 	if (isPaused) {
 		classNames = classNames + ' ' + styles['pause-button--paused'];
 	}
+	const buttonLabel = `${isPaused ? 'Play' : 'Pause'} "${title}" image slideshow`;
 	return (
 		<div className={styles['img-block']}>
-			<button className={classNames} aria-label="control animation" onClick={togglePause} />
+			<button className={classNames} aria-label={buttonLabel} onClick={togglePause} />
 			{images.map((image, index) => (
 				<Image key={index} linkUrl={link} imageUrl={image} fadeIndex={index} isPaused={isPaused} />
 			))}
